f8e49f9ff63b789ff4cb11b296bf2f1cb7ded8da
Davide-Morellato <morellato.davide@yahoo.it>
2024-07-25 12:16:46 +0200

    ADD: kart reaction events

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //
-// # FASE 1: PREPARAZIONE DEGLI ELEMENTI BASE DELLA GRIGLIA DI GIOCO
+// FASE 1: PREPARAZIONE DEGLI ELEMENTI BASE DELLA GRIGLIA DI GIOCO
 //
 
 //
@@ -16,33 +16,33 @@ const gridMatrix = [
     ['', 'water', '', '', '', '', 'grass'],
     ['fence', '', '', '', '', '', ''],
     ['', '', '', '', '', '', ''],
-  ];
+];
   
-  //console.table(gridMatrix) <-Consente di vedere in console come è strutturata la griglia
-  
-  //
-  //recupero l'elemento con classe .grid dal DOM
-  //perchè successivamente devo costruire la griglia al suo interno
-  //e riempirla usando l'array gridMatrix
-  const grid = document.querySelector('.grid');
-  
-  
-  //
-  //La posizione del Kart deve essere dinamica,
-  //quindi dichiaro una variabile a cui assegno un oggetto con coordinate x e y per la posizione del Kart nella griglia
-  const kartPosition = {
+//console.table(gridMatrix) <-Consente di vedere in console come è strutturata la griglia
+
+//
+//recupero l'elemento con classe .grid dal DOM
+//perchè successivamente devo costruire la griglia al suo interno
+//e riempirla usando l'array gridMatrix
+const grid = document.querySelector('.grid');
+
+
+//
+//La posizione del Kart deve essere dinamica,
+//quindi dichiaro una variabile a cui assegno un oggetto con coordinate x e y per la posizione del Kart nella griglia
+const kartPosition = {
     y: 8,
     x: 3,
-  };
+};
   
-  //
-  //funzione per mostrare la griglia con al suo interno tutti gli elementi dell'array gridMatrix
-  function renderGrid() {
-  
-      //prima di riempirire la griglia, svuotala (così ad ogni evento di movimento non viene duplicata)
-      grid.innerHTML = '';
-      
-      
+//
+//funzione per mostrare la griglia con al suo interno tutti gli elementi dell'array gridMatrix
+function renderGrid() {
+
+    //prima di riempirire la griglia, svuotala (così ad ogni evento di movimento non viene duplicata)
+    grid.innerHTML = '';
+    
+    
     //recupero ogni singola riga dell'array tramite un ciclo forEach
     //in cui dichiaro come parametro della funzione la singola riga (singleRow)
     gridMatrix.forEach(function (singleRow) {
@@ -70,25 +70,127 @@ const gridMatrix = [
         grid.appendChild(singleCell);
       });
     });
-  }
+}
   
-  //
-  //dichiaro una funzione per posizionare il kart
-  function placeKart() {
+//
+//dichiaro una funzione per posizionare il kart
+function placeKart() {
     //prendo i valori delle coordinate x e y dell'oggetto kartPosition e li assegno all'array (gridMatrix),
     //assegnando la classe '.kart' per visualizzarlo
     gridMatrix[kartPosition.y][kartPosition.x] = 'kart';
-  }
+}
   
-  //
-  //Raggruppo le funzioni di preparazione
-  function renderingFunction() {
+//
+//Raggruppo le funzioni di preparazione
+function renderingFunction() {
     //invoco la funzione placeKart()
     placeKart();
-  
+
     //invoco la funzione renderGrid()
     renderGrid();
-  }
+}
 
-  renderingFunction();
-  
+
+//
+// FASE 2: MOVIMENTO DEL KART
+//
+
+//
+//recupero i singoli bottoni dal DOM (document.querySelector)
+const leftButton = document.querySelector('.left-button');
+const rightButton = document.querySelector('.right-button');
+
+//
+//aggiungo ai bottoni un evento di ascolto al 'click'
+//(addEventListner -> metodo che accetta due parametri: il tipo di evento; una funzione con le operazioni da eseguire per quell'evento)
+leftButton.addEventListener('click', function () {
+    // console.log('left');
+
+    //invoco la funzione moveKart() e gli assegno il parametro 'left'
+    moveKart('left');
+});
+
+rightButton.addEventListener('click', function () {
+    // console.log('right');
+
+    //invoco la funzione moveKart() e gli assegno il parametro 'right'
+    moveKart('right');
+});
+
+//
+//aggiungo un evento alla pressione di un tasto 'keyup',
+//assegnando alla funzione un parametro per quell'evento (event)
+document.addEventListener('keyup', function (event) {
+    // console.log(event.key); <- 'evento.key' consente di leggere in console il tasto premuto: FRECCIA DESTRA = ArrowRight; FRECCIA SINISTRA = ArrowLeft
+
+    //dichiaro il metodo switch()
+    //che permette di analizzare il valore di un evento e reagire in base alle direttive imposte
+    switch (event.key) {
+        //nel caso in cui venga premuta la freccia sinistra:
+        //spostati a sinitra;
+        //e interrompi il ciclo di ricerca (break);
+        case 'ArrowLeft':
+        //richiamo la funzione movekart() per indicare in quale posizione deve spostarsi il kart
+        moveKart('left');
+        // console.log('sn')
+        break;
+
+        //nel caso in cui venga premuta la freccia destra:
+        //spostati a destra;
+        //e interrompi il ciclo di ricerca;
+        case 'ArrowRight':
+        //richiamo la funzione movekart() per indicare in quale posizione deve spostarsi il kart
+        moveKart('right');
+        // console.log('dx')
+        break;
+
+        //per tutti gli altri casi, ritorna al tuo stato iniziale e non eseguire nulla (default: return)
+        default: return;
+    }
+});
+
+
+//
+//dichiaro una funzione che mi permette di indicare la direzione di movimento del kart
+function moveKart(directionKart) {
+
+    //prima di posizionare il kart, rimuovilo dalla cella precedente
+    //assegnando alle coordinate x e y stringa vuota
+    gridMatrix[kartPosition.y][kartPosition.x] = '';
+
+
+    //dichiaro il metodo switch() a cui assegno il parametro di riferimento
+    //aggiornando le coordinate del kart in base all'evento (click o keyup)
+    switch (directionKart) {
+        //nel caso in cui si muova a sinistra:
+        //per evitare che il kart esca dalla griglia, eseguo un controllo:
+            //SE la coordinata x è MAGGIORE di 0
+                //decrementa la coordinata x
+        //e interrompi il ciclo di ricerca (break);
+        case 'left':
+        if(kartPosition.x > 0){
+            kartPosition.x--;
+        }
+        break;
+
+        //nel caso in cui si muova a destra:
+        //per evitare che il kart esca dalla griglia, eseguo un controllo:
+            //SE la coordinata x è MINORE di 6 (numero massimo di celle lungo l'asse x)
+                //incrementa la coordinata x
+        //e interrompi il ciclo di ricerca (break);
+        case 'right':
+        if(kartPosition.x < 6){
+            kartPosition.x++;
+        }
+        break;
+    }
+
+    //invoco la funzione di preparazione della griglia,
+    //in questo modo verrà aggiornata alla nuova posizione del Kart
+    renderingFunction();
+}
+
+//
+//invoco la funzione renderingFunction()
+renderingFunction();
+
